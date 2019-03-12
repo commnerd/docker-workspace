7485bda0e21b65697a67c41f6bc093422b21598f
Michael J. Miller <miller_m@cde.state.co.us>
2019-03-12 16:39:15 +0000

    Finished untested system build

--- controller/backend/api.go
+++ controller/backend/api.go
@@ -6,33 +6,31 @@ import (
     "text/template"
     "io/ioutil"
     "net/http"
-    // "os/exec"
-    // "strings"
+    "os/exec"
     "bytes"
-    // "fmt"
     "log"
-    // "os"
+    "fmt"
+    "os"
 )
 
-const ProxyTemplate = `{{ .EnvBasePath }} {{ .Port }} {{ .SiteBasePath }}`
+const ProxyTemplate = `location {{ .EnvBasePath }} { http://localhost:{{ .Port }}{{ .SiteBasePath }}; }`
 
 type Proxy struct{
-	Name string         `json:"name"`
 	Port string         `json:"port"`
 	EnvBasePath string  `json:"env_base_path"`
 	SiteBasePath string `json:"site_base_path"`
 }
 
-var Proxies []Proxy
+var Proxies map[string]*Proxy
 
 func main() {
-    Proxies = make([]Proxy, 0)
+    Proxies = make(map[string]*Proxy)
 
     r := mux.NewRouter()
     r.HandleFunc("/", Hello).Methods("GET").Name("index")
     r.HandleFunc("/proxies", ProxyList).Methods("GET").Name("proxies.list")
     r.HandleFunc("/proxies", ProxyStore).Methods("POST").Name("proxies.store")
-    r.HandleFunc("/proxies/{name}", ProxyDelete).Methods("DELETE").Name("proxies.delete")
+    r.HandleFunc("/proxies", ProxyDelete).Methods("DELETE").Name("proxies.delete")
 
     log.Fatalln(http.ListenAndServe(":8000", r))
 }
@@ -42,64 +40,96 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 }
 
 func ProxyList(w http.ResponseWriter, r *http.Request) {
+    list, err := json.Marshal(ProxyList)
+    if err != nil {
+        http.Error(w, err.Error(), 500)
+        return
+    }
 
+    fmt.Fprintf(w, string(list))
 }
 
 func ProxyStore(w http.ResponseWriter, r *http.Request) {
-	// Read body
-	b, err := ioutil.ReadAll(r.Body)
-	defer r.Body.Close()
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
+	proxy, err := getProxyFromRequest(r)
+    if(err != nil) {
+        http.Error(w, err.Error(), 500)
+        return
+    }
 
-	// Unmarshal
-	var proxy Proxy
-	err = json.Unmarshal(b, &proxy)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-	}
+    Proxies[proxy.EnvBasePath] = &proxy
 
-/*
-    tmpl, err := template.New("proxy").Parse(ProxyTemplate)
-    http.Error(w, err.Error(), 500)
-    tmpl.Execute(tmpl, &proxy)
-*/
-    Proxies = append(Proxies, proxy)
-
-    /*
-	f, err := os.Create("/etc/nginx/sites-enabled/"+proxy.Name+".conf")
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-	}
-    */
+    tmpl, err := template.ParseFiles("/root/.nginx.default.template")
+    if(err != nil) {
+        http.Error(w, err.Error(), 500)
+        return
+    }
 
     entries := writeEntries()
+    replacement := struct{
+        ProxyList string
+    }{
+        ProxyList: entries,
+    }
 
-    /*
+    file, err := os.OpenFile("/etc/nginx/sites-available/default", os.O_RDWR, 0644)
+    if(err != nil) {
+        http.Error(w, err.Error(), 500)
+        return
+    }
+    defer file.Close()
+
+    err = tmpl.Execute(file, replacement)
+    if(err != nil) {
+        http.Error(w, err.Error(), 500)
+        return
+    }
+
 	cmd := exec.Command("service", "nginx", "restart")
 	err = cmd.Run()
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 	}
-    */
 
-	w.WriteHeader(http.StatusOK)
-    w.Write([]byte(entries))
-	// fmt.Fprintf(w, "{ \"status\": \"success\" }\n")
+	fmt.Fprintf(w, "{ \"status\": \"success\" }\n")
 }
 
 func ProxyDelete(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World!\n"))
+    proxy, err := getProxyFromRequest(r)
+    if(err != nil) {
+        http.Error(w, err.Error(), 500)
+        return
+    }
+
+	delete(Proxies, proxy.EnvBasePath)
+
+    fmt.Fprintf(w, "{ \"status\": \"success\" }\n")
+}
+
+func getProxyFromRequest(r *http.Request) (Proxy, error) {
+    // Read body
+    b, err := ioutil.ReadAll(r.Body)
+    defer r.Body.Close()
+    if err != nil {
+        return Proxy{}, err
+    }
+
+    // Unmarshal
+    var proxy Proxy
+    err = json.Unmarshal(b, &proxy)
+    if err != nil {
+        return Proxy{}, err
+    }
+
+    return proxy, nil
 }
 
 func writeEntries() string {
     var buf bytes.Buffer
-    for proxy := range(Proxies) {
+    for _, proxy := range(Proxies) {
         tpl := template.Must(template.New("ProxyTemplate").Parse(ProxyTemplate))
         tpl.Execute(&buf, proxy)
+
         buf.Write([]byte("\n\t\t\t"))
     }
-    return string(string(buf.Bytes()))
+    return string(buf.Bytes())
 }
