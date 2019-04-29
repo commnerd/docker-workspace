b4d2a93f9d178590204d82ad01b37aa935ebe546
Michael J. Miller <miller_m@cde.state.co.us>
2019-04-29 20:17:02 +0000

    Fixed a couple of install bugs

--- install.sh
+++ install.sh
@@ -3,27 +3,27 @@
 DEST="${HOME}/.bash_profile"
 TERMINAL_TOOL=""
 NETWORK=""
-PORTS="-p 80:80"
-NAME="--name workspace"
+PORTS=" -p 80:80"
+NAME=" --name workspace"
 VERSION="latest"
 if [ "$1" ]; then VERSION="$1"; fi
-VOLUMES="-v ${HOME}/Workspace:/home/commnerd/Workspace -v ${HOME}/.ssh:/root/.ssh -v /var/run/docker.sock:var/run/host.docker.sock"
-IMAGE="commnerd/workspace:${VERSION}"
+VOLUMES=" -v ${HOME}/Workspace:/home/commnerd/Workspace -v ${HOME}/.ssh:/root/.ssh -v /var/run/docker.sock:/var/run/host.docker.sock"
+IMAGE=" commnerd/workspace:${VERSION}"
 
 case "$OSTYPE" in
 "linux-gnu")
     PORTS=""
-    NETWORK="--network host"
+    NETWORK=" --network host"
     ;;
 "msys")
-    VOLUMES="-v C:\\\\Users\\\\miller_m\\\\Workspace:/home/commnerd/Workspace -v C:\\\\Users\\\\miller_m\\\\.ssh:/root/.ssh -v //var/run/docker.sock:/var/run/host.docker.sock -v C:\\\\Users\\\\miller_m\\\\scripts:/home/commnerd/scripts"
+    VOLUMES=" -v C:\\\\Users\\\\miller_m\\\\Workspace:/home/commnerd/Workspace -v C:\\\\Users\\\\miller_m\\\\.ssh:/root/.ssh -v //var/run/docker.sock:/var/run/host.docker.sock -v C:\\\\Users\\\\miller_m\\\\scripts:/home/commnerd/scripts"
     TERMINAL_TOOL="winpty "
     ;;
 "win32")
-    VOLUMES="-v C:\\\\Users\\\\miller_m\\\\Workspace:/home/commnerd/Workspace -v C:\\\\Users\\\\miller_m\\\\.ssh:/root/.ssh -v //var/run/docker.sock:/var/run/host.docker.sock -v C:\\\\Users\\\\miller_m\\\\scripts:/home/commnerd/scripts"
+    VOLUMES=" -v C:\\\\Users\\\\miller_m\\\\Workspace:/home/commnerd/Workspace -v C:\\\\Users\\\\miller_m\\\\.ssh:/root/.ssh -v //var/run/docker.sock:/var/run/host.docker.sock -v C:\\\\Users\\\\miller_m\\\\scripts:/home/commnerd/scripts"
     TERMINAL_TOOL="winpty "
     ;;
 esac
 
-echo "alias run-workspace='docker run -d --privileged --restart always ${NAME} ${PORTS} ${VOLUMES} ${IMAGE}'" >> ${DEST}
+echo "alias run-workspace='docker run -d --privileged --restart always ${NETWORK}${NAME}${PORTS}${VOLUMES}${IMAGE}'" >> ${DEST}
 echo "alias workspace='${TERMINAL_TOOL}docker exec -it -u commnerd --workdir //home/commnerd workspace bash'" >> ${DEST}
